0816e9fb0c8c6fdc030752f86f23b488d3adb906
Muneeb <92539069+136muneebshouket@users.noreply.github.com>
2024-04-20 00:34:56 +0500

    again

--- erp_cash_carry/src/app/page.js
+++ erp_cash_carry/src/app/page.js
@@ -1,10 +1,6 @@
 import Image from "next/image";
 import Header from "@/components/home_components/Header";
 import About from "@/components/home_components/About";
-import Choose_us from "@/components/home_components/Choose_us";
-import Services from "@/components/home_components/Services";
-import Services_2 from "@/components/home_components/Services_2";
-import Team from "@/components/home_components/Team";
 import Contact from "@/components/home_components/Contact";
 import Footer from "@/components/home_components/Footer";
 // import Navbarr from "../../components/navbar/Navbarr";
@@ -14,10 +10,6 @@ export default function Home() {
     <>
     <Header/>
     <About/>
-    {/* <Choose_us/> */}
-    {/* <Services/> */}
-    {/* <Services_2/> */}
-    {/* <Team/> */}
     <Contact/>
     <Footer/>
     
