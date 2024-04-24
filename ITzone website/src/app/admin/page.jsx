678d333c554ae4f363d7ae23b85224abb0feb664
PirateCoderz <118294175+PirateCoderz@users.noreply.github.com>
2024-04-24 13:35:20 +0500

    Revert "admin portal"
    This reverts commit 4c3c4e6b3fdac01b2583fcaf0918342e622415c6.

---
+++ ITzone website/src/app/admin/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <div className='main_admin_page relative'>
+     <nav className='ab'>
+
+     </nav>
+
+    </div>
+
+      
+    </>
+  )
+}
+
+export default page
